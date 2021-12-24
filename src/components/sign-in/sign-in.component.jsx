c13fcf3fafcf50835229b08bf8daf4829bafc8c5
Hangga Rizki <hangga@majoo.id>
2021-12-24 15:33:46 +0700

    add new form component, button component, sign in section and routes

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    handleSubmit = event => {
+        event.prefentDefault();
+        this.setState({
+            email: '', password: '',
+        });
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        const { email, password } = this.state;
+        return(
+            <div className="sign-in">
+                <h2>I already have account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name="email"
+                        type="email"
+                        handleChange={this.handleChange}
+                        value={email}
+                        required
+                        label="Email"
+                    />
+                    <FormInput
+                        name="password"
+                        type="password"
+                        handleChange={this.handleChange}
+                        value={password}
+                        required
+                        label="Password"
+                    />
+
+                    <CustomButton type="submit">
+                        Sign In
+                    </CustomButton>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
